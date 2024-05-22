845b76692cfb272d891a0bc3b7548461a093fb33
Zapzoop <turtlemaster700s@gmail.com>
2024-05-22 20:00:15 +0530

    Sort of buggy implementation for crafting

--- Mechanics/Crafting/Draggable/draggable.gd
+++ Mechanics/Crafting/Draggable/draggable.gd
@@ -1,23 +1,44 @@
-extends TextureRect
+extends RigidBody2D
+
+@onready var icon = $Icon
+@onready var detection = $Detection
+@onready var mouse_det = $Mouse_Detection
 
 var offset
+var id
 
-func _on_gui_input(event):
-	var mouse_pos = get_global_mouse_position()
-	var tween = get_tree().create_tween()
-	var corrected
+var selected = false
+var inside
+
+var corrected
+var mouse_pos
+
+var inserted = false
+
+func _ready():
+	#set_scale_me()
+	pass
+
+func _process(delta):
 	if Input.is_action_just_pressed("left_mouse"):
+		if inside:
+			selected = true
+		mouse_pos = get_global_mouse_position()
 		offset = mouse_pos - self.global_position
-	if Input.is_action_pressed("left_mouse"):
+	if Input.is_action_pressed("left_mouse") and selected and !inserted:
+		mouse_pos = get_global_mouse_position()
 		corrected = mouse_pos-offset
+		var tween = get_tree().create_tween()
 		tween.tween_property(self,"position",corrected,0.1).set_ease(Tween.EASE_OUT)
 
+func _on_mouse_detection_mouse_entered():
+	inside = true
+	self.modulate = Color(1,1,0,1)
+	#set_scale_me()
+	#print(str(scale) + " my scale")
 
-func _on_mouse_entered():
+func _on_mouse_detection_mouse_exited():
+	inside = false
 	if !Input.is_action_pressed("left_mouse"):
-		self.modulate = Color(1,1,0,1)
-
-
-func _on_mouse_exited():
-	if !Input.is_action_pressed("left_mouse"):
-		self.modulate = Color(1,1,1,1)
+		selected = false
+	self.modulate = Color(1,1,1,1)
